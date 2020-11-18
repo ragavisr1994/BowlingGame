643d2207223d8eeaab36de5852aec249109b2e37
ragavisr1994 <Ragavi.SankaranRameshbabu3@T-Mobile.com>
2020-11-17 22:32:03 -0800

    One Spare with three rolls

--- test/testbowlinggame.py
+++ test/testbowlinggame.py
@@ -10,16 +10,29 @@ class testbowlinggame(unittest.TestCase):
         self.assertEqual(self.game.score ,0)
         print(str(self.game.score))
 
-    """ def test_recordrollingofball(self):
-        self.game.roll(8)
-        self.assertEquals(self.game.total_score, 8)
-        print(str(self.game.score)) """
-
     def test_allgutters(self):
-        self._roll_many(0, 20)
+        self.roll_many(0, 20)
         self.assertEquals(0, self.game.roll_score())
         print(str(self.game.score))
+  
+    def test_allones(self):
+        self.roll_many(1, 20)
+        self.assertEquals(20, self.game.roll_score())
+        print(str(self.game.score))
 
-    def _roll_many(self, pins, num):
+    def test_onesparewiththreerolls(self):
+        self.roll_spare()
+        self.game.roll(5)
+        self.roll_many(0, 17)
+        self.assertEqual(20, self.game.roll_score())
+
+    def roll_many(self, pins, num):
         for a in range(num):
             self.game.roll(pins)
+
+    def roll_spare(self):
+        self.game.roll(5)
+        self.game.roll(5)
+
+    def roll_strike(self):
+        self.game.roll(10)
