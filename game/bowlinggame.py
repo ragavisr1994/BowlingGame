92c143c38ea2c64fc33b9e36b7669733350514fb
ragavisr1994 <Ragavi.SankaranRameshbabu3@T-Mobile.com>
2020-11-17 19:26:20 -0800

    All gutters case

--- game/bowlinggame.py
+++ game/bowlinggame.py
@@ -1,4 +1,28 @@
 class bowlinggame():
     def __init__(self):
-        self.frames = []
+        self.rolls = []
         self.score = 0
+
+    def roll(self, pins):
+        self.rolls.append(pins)
+
+    def roll_score(self):
+        score = 0
+        roll_index = 0
+        for frame in range(10):
+            if self.strike(roll_index):
+                score += 10 + self.rolls[roll_index+1] + self.rolls[roll_index+2]
+                roll_index += 1
+            elif self.spare(roll_index):
+                score += 10 + self.rolls[roll_index+2]
+                roll_index += 2
+            else:
+                score += self.rolls[roll_index] + self.rolls[roll_index+1]
+                roll_index += 2
+        return score
+
+    def spare(self, roll_index):
+        return self.rolls[roll_index] + self.rolls[roll_index+1] == 10 
+
+    def strike(self, roll_index):
+        return self.rolls[roll_index] == 10
--- test/testbowlinggame.py
+++ test/testbowlinggame.py
@@ -2,10 +2,24 @@ import unittest
 from game.bowlinggame import bowlinggame
 
 class testbowlinggame(unittest.TestCase):
-
+    
     def setUp(self):
         self.game = bowlinggame()
 
     def test_scorezero(self):
         self.assertEqual(self.game.score ,0)
         print(str(self.game.score))
+
+    """ def test_recordrollingofball(self):
+        self.game.roll(8)
+        self.assertEquals(self.game.total_score, 8)
+        print(str(self.game.score)) """
+
+    def test_allgutters(self):
+        self._roll_many(0, 20)
+        self.assertEquals(0, self.game.roll_score())
+        print(str(self.game.score))
+
+    def _roll_many(self, pins, num):
+        for a in range(num):
+            self.game.roll(pins)
